8e0d5634a1176c8eb05c40cbdf2756fab39caebf
Morgan Ericsson <morganericsson@gmail.com>
2025-04-29 13:34:11 +0200

    first version

---
+++ calc_server.py
@@ -0,0 +1,18 @@
+from mcp.server.fastmcp import FastMCP
+
+app = FastMCP(
+    title="Simple Calculator",
+    description="A server to do simple calculations", 
+    version="0.1",
+)
+
+@app.tool()
+def add(v1:int, v2:int) -> int:
+    """
+    Adds to integers and returns the result
+    """
+
+    return v1 + v2
+
+if __name__ == "__main__":
+    app.run()
